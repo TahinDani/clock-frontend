0aa9eb2b71e5e2471ee463e4dac97cdc2d944626
Daniel Tahin <tahin.daniel@gmail.com>
2020-05-21 09:07:24 +0200

    Moved the logic to Clock component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import Clock from './components/Clock'
-import useInterval from './hooks/useInterval'
 import './App.css';
 
 function App() {
-	const [date, setDate] = useState({})
-	const [delay, setDelay] = useState(1000)
-	const [options, setOptions] = useState({
-		locale: "hu-HU", 
-		type: "full", 
-		is12: "false", 
-		showSeconds: "true"
-	})
-
-	const queryString = `?type=${options.type}&locale=${options.locale}&is12=${options.is12}&showSeconds=${options.showSeconds}`
-	const updateDate =  useCallback(async () => {
-		const response = await fetch(`http://localhost:3001/currentTime${queryString}`)
-		const newDate = await response.json()
-		setDate(newDate)
-	}
-	, [queryString])
-
-	const onOptionChange = (e) => {
-		if (options[e.currentTarget.name] !== e.currentTarget.value) {
-			setOptions({...options, [e.currentTarget.name]:e.currentTarget.value})
-		}
-	}
-
-	useEffect(()=>{
-		if (options.type === "date") {
-			setDelay(300000) // 5 minutes
-		} else if (options.showSeconds === "false") {
-			setDelay(3000)
-		} else {
-			setDelay(1000)
-		}
-	}, [options])
-
-	useEffect(()=> {
-		console.log("useEffect")
-		updateDate()
-	}, [options, updateDate])
-
-	useInterval(() => {
-		updateDate()
-	}, delay);
 
 	return (
 		<div className="App">
-			<Clock responseDate={date} onChange={onOptionChange}/>
+			<Clock />
 		</div>
 	);
 }
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,46 +1,88 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import useInterval from '../hooks/useInterval'
 import '../styles/Clock.css'
 
-const Clock = ({responseDate, onChange}) => {
+const Clock = () => {
+	const [date, setDate] = useState({})
+	const [delay, setDelay] = useState(1000)
+	const [options, setOptions] = useState({
+		locale: "hu-HU", 
+		type: "full", 
+		is12: "false", 
+		showSeconds: "true"
+	})
+	const hourMinute = date.time?.slice(0, -3)
+	const second = date.time?.slice(-3)
+	console.log(hourMinute, second);
 
-	const date = responseDate.date
-	const time = responseDate.time
+	const updateDate =  useCallback(async () => {
+		const queryString = `?type=${options.type}&locale=${options.locale}&is12=${options.is12}&showSeconds=${options.showSeconds}`
+		const response = await fetch(`http://localhost:3001/currentTime${queryString}`)
+		const newDate = await response.json()
+		setDate(newDate)
+	}
+	, [options])
 
+	const onOptionChange = (e) => {
+		if (options[e.currentTarget.name] !== e.currentTarget.value) {
+			setOptions({...options, [e.currentTarget.name]:e.currentTarget.value})
+		}
+	}
+
+	useEffect(()=>{
+		if (options.type === "date") {
+			setDelay(300000) // 5 minutes
+		} else if (options.showSeconds === "false") {
+			setDelay(3000)
+		} else {
+			//setDelay(1000)
+			setDelay(60000)
+		}
+	}, [options])
+
+	useEffect(()=> {
+		updateDate()
+	}, [options, updateDate])
+
+	useInterval(() => {
+		updateDate()
+	}, delay);
+	
 	return (
 		<div className="Clock">
 			<div className="Clock-display">
-				<div>{time}</div>
-				<div>{date}</div>
+				<div className="Clock-time">{hourMinute}<span>{second}</span></div>
+				<div className="Clock-date">{date.date}</div>
 			</div>
 			<div className="Clock-settings">
-				<div className="option">
+				<div className="Clock-option">
 					<ButtonGroup color="secondary" aria-label="outlined secondary button group">
-						<Button onClick={onChange} name="type" value="full">mind</Button>
-						<Button onClick={onChange} name="type" value="date">dátum</Button>
-						<Button onClick={onChange} name="type" value="time">idő</Button>
+						<Button onClick={onOptionChange} name="type" value="full">mind</Button>
+						<Button onClick={onOptionChange} name="type" value="date">dátum</Button>
+						<Button onClick={onOptionChange} name="type" value="time">idő</Button>
 					</ButtonGroup>
 				</div>
 
-				<div className="option">
+				<div className="Clock-option">
 					<ButtonGroup color="secondary" aria-label="outlined secondary button group">
-						<Button onClick={onChange} name="locale" value="hu-HU">magyar</Button>
-						<Button onClick={onChange} name="locale" value="en-US">angol</Button>
+						<Button onClick={onOptionChange} name="locale" value="hu-HU">magyar</Button>
+						<Button onClick={onOptionChange} name="locale" value="en-US">angol</Button>
 					</ButtonGroup>
 				</div>
 
-				<div className="option">
+				<div className="Clock-option">
 					<ButtonGroup color="secondary" aria-label="outlined secondary button group">
-						<Button onClick={onChange} name="is12" value="true">12 óra</Button>
-						<Button onClick={onChange} name="is12" value="false">24 óra</Button>
+						<Button onClick={onOptionChange} name="is12" value="true">12 óra</Button>
+						<Button onClick={onOptionChange} name="is12" value="false">24 óra</Button>
 					</ButtonGroup>
 				</div>
 
-				<div className="option">
+				<div className="Clock-option">
 					<ButtonGroup color="secondary" aria-label="outlined secondary button group">
-						<Button onClick={onChange} name="showSeconds" value="true">mp</Button>
-						<Button onClick={onChange} name="showSeconds" value="false">no mp</Button>
+						<Button onClick={onOptionChange} name="showSeconds" value="true">mp</Button>
+						<Button onClick={onOptionChange} name="showSeconds" value="false">no mp</Button>
 					</ButtonGroup>
 				</div>
 			</div>
